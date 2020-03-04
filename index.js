3bed3a50bfe0f517f9968673069cf603ef95cce9
Brigham Mueller <brigmu@gmail.com>
2020-03-04 14:26:51 -0800

    added ability to view departments and add departments. Also added ability to update emloyee roles

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const init = () => {
             type: 'list',
             message: 'What would you like to do?',
             name: 'userChoice',
-            choices: ['View all employees', 'Add an employee', 'View all roles', 'Add a role', 'View all departments', 'Add a department', 'Update employee roles']
+            choices: ['View all employees', 'Add an employee', 'View all roles', 'Add a role', 'View all departments', 'Add a department', 'Update employee roles', 'Exit']
         }
     ])
     .then((response) =>{
@@ -39,6 +39,18 @@ const init = () => {
                 break;
             case 'Add a role':
                 addARole();
+                break;
+            case 'View all departments':
+                viewAllDepartments()
+                break;
+            case 'Add a department':
+                addADepartment();
+                break;
+            case 'Update employee roles':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                connection.end();
                 break;
             default:
                 console.log('Something broke');
@@ -158,5 +170,98 @@ const addARole = () => {
             console.log('Added role');
             init();
         })
+    })
+}
+
+const viewAllDepartments = () => {
+    connection.query('SELECT id, name FROM department', (err, results) =>{
+        if(err) {
+            throw err;
+        }
+        console.table(results);
+        init();
+    })
+}
+
+const addADepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'input', 
+            message: 'Enter the name of new department',
+            name: 'userDepartment'
+        }
+    ])
+    .then((results) => {
+        connection.query(`INSERT into department(name) values('${results.userDepartment}')`, (err, results) => {
+            if(err){
+                throw err;
+            }
+            console.log('Department added');
+            init();
+        })
+    })
+}
+
+const updateEmployeeRole = () => {
+    let choices = [];
+    let employees = [];
+    let roles = [];
+    
+    connection.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name as department FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id', (err, results) =>{
+        if(err){
+            throw err;
+        }
+
+        for(let i = 0; i < results.length; i++){
+            employees.push(results[i]);
+            choices.push(`${results[i].first_name} ${results[i].last_name}`);
+        }
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: 'Choose employee whose role you would like to update',
+                name: 'userChoice',
+                choices: choices
+            }
+        ])
+        .then((results) => {
+            let roleChoices = [];
+            let employee = results.userChoice;
+            let empIndex = choices.indexOf(employee);
+            let firstName = employees[empIndex].first_name;
+            connection.query('SELECT role.id, role.title, role.salary, department.name as department FROM role INNER JOIN department ON role.department_id = department.id', (err, results) =>{
+                if(err) {
+                    throw err;
+                }
+                for(let i = 0; i < results.length; i++){
+                    roles.push(results[i]);
+                    roleChoices.push(results[i].title);
+                }
+                // console.table(roles);
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        message: `Which role would you like to change ${employee} to?`,
+                        name: 'newRole',
+                        choices: roleChoices
+                    }
+                ])
+                .then((results) => {
+                    const index = roleChoices.indexOf(results.newRole);
+                    console.table(roles);
+                    console.log(index);
+                    const roleId = roles[index].id;
+                    connection.query(`UPDATE employee SET role_id = ${roleId} WHERE first_name = '${firstName}'`, (err, results) => {
+                        if(err){
+                            throw err;
+                        }
+                        console.log('Updated employee role');
+                        init();
+                    })
+                })
+            })
+        })
+
     })
 }
