ea9a3cf6a17245cc928884de837605858e8f038d
Brigham Mueller <brigmu@gmail.com>
2020-03-08 15:58:18 -0700

    added functionality to remove an employee. Added checks to trim excess white space from user input and check whether nothing was inputted or the input was longer than database can hold. added functionality to allow user to remove a role and then update all employees who would be affected by that removal, either all at once or individually,

--- index.js
+++ index.js
@@ -22,11 +22,10 @@ const init = () => {
             type: 'list',
             message: 'What would you like to do?',
             name: 'userChoice',
-            choices: ['View all employees', 'Add an employee', 'View all roles', 'Add a role', 'View all departments', 'Add a department', 'Update employee roles', 'Exit']
+            choices: ['View all employees', 'Add an employee', 'Remove an employee', 'View all roles', 'Add a role', 'Remove a role', 'View all departments', 'Add a department', 'Update employee roles', 'Exit']
         }
     ])
     .then((response) =>{
-        console.log(response.userChoice);
         switch(response.userChoice){
             case 'View all employees':
                 viewAllEmployees();
@@ -34,11 +33,17 @@ const init = () => {
             case 'Add an employee':
                 addAnEmployee();
                 break;
+            case 'Remove an employee':
+                removeAnEmployee();
+                break;
             case 'View all roles':
                 viewAllRoles();
                 break;
             case 'Add a role':
                 addARole();
+                break;
+            case 'Remove a role':
+                removeARole();
                 break;
             case 'View all departments':
                 viewAllDepartments()
@@ -58,7 +63,7 @@ const init = () => {
     })
 }
 
-init();
+
 
 const viewAllEmployees = () => {
     connection.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name as department FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id', (err, results) =>{
@@ -102,18 +107,25 @@ const addAnEmployee = () => {
         }
     ])
     .then((results => {
-        console.table(results);
-        const index = choices.indexOf(results.userRole);
-        const roleId = roles[index].id;
-        console.log(roleId);
-        
-        connection.query(`INSERT into employee(first_name, last_name, role_id) values('${results.userFirstName}','${results.userLastName}', ${roleId})`, (err, results) => {
-            if(err){
-                throw err;
-            };
-            console.log('Added employee');
+        const checkUser = checkUserInputs(results.userFirstName, results.userLastName);
+        console.log(checkUser);
+        if (!checkUser) {
+            const index = choices.indexOf(results.userRole);
+            const roleId = roles[index].id;
+            console.log(roleId);
+
+            connection.query(`INSERT into employee(first_name, last_name, role_id) values(?, ?, ?)`, [results.userFirstName.trim(), results.userLastName.trim(), roleId], (err, results) => {
+                if (err) {
+                    throw err;
+                };
+                console.log('Added employee');
+                init();
+            })
+        }
+        else {
+            console.log('Invalid input');
             init();
-        })
+        }
     }))
 }
 
@@ -159,17 +171,23 @@ const addARole = () => {
         }
     ])
     .then((results) => {
-        const index = choices.indexOf(results.userDepartment);
-        const departmentId = departments[index].id;
-        console.log(departmentId);
+        const checkUser = checkUserInputs(results.userTitle, results.userSalary);
+        if (!checkUser) {
+            const index = choices.indexOf(results.userDepartment);
+            const departmentId = departments[index].id;
+            console.log(departmentId);
 
-        connection.query(`INSERT into role(title, salary, department_id) values('${results.userTitle}','${results.userSalary}', ${departmentId})`, (err, results) => {
-            if(err){
-                throw err;
-            };
-            console.log('Added role');
+            connection.query(`INSERT into role(title, salary, department_id) values(?, ?, ?)`, [results.userTitle.trim(), results.userSalary.trim(), departmentId], (err, results) => {
+                if (err) {
+                    throw err;
+                };
+                console.log('Added role');
+                init();
+            })
+        } else {
+            console.log('Invalid Input');
             init();
-        })
+        }
     })
 }
 
@@ -192,13 +210,19 @@ const addADepartment = () => {
         }
     ])
     .then((results) => {
-        connection.query(`INSERT into department(name) values('${results.userDepartment}')`, (err, results) => {
-            if(err){
-                throw err;
-            }
-            console.log('Department added');
+        const checkUser = checkUserInputs(results.userDepartment);
+        if (!checkUser) {
+            connection.query(`INSERT into department(name) values(?)`, [results.userDepartment.trim()], (err, results) => {
+                if (err) {
+                    throw err;
+                }
+                console.log('Department added');
+                init();
+            })
+        } else {
+            console.log('Invalid Input');
             init();
-        })
+        }
     })
 }
 
@@ -230,6 +254,7 @@ const updateEmployeeRole = () => {
             let employee = results.userChoice;
             let empIndex = choices.indexOf(employee);
             let firstName = employees[empIndex].first_name;
+            let lastName = employees[empIndex].last_name;
             connection.query('SELECT role.id, role.title, role.salary, department.name as department FROM role INNER JOIN department ON role.department_id = department.id', (err, results) =>{
                 if(err) {
                     throw err;
@@ -252,7 +277,7 @@ const updateEmployeeRole = () => {
                     console.table(roles);
                     console.log(index);
                     const roleId = roles[index].id;
-                    connection.query(`UPDATE employee SET role_id = ${roleId} WHERE first_name = '${firstName}'`, (err, results) => {
+                    connection.query(`UPDATE employee SET role_id = ? WHERE first_name = ? and last_name = ?`, [roleId, firstName, lastName], (err, results) => {
                         if(err){
                             throw err;
                         }
@@ -265,3 +290,210 @@ const updateEmployeeRole = () => {
 
     })
 }
+
+const removeAnEmployee = () => {
+    let choices = [];
+    connection.query('SELECT first_name, last_name FROM employee', (err, results) => {
+        if (err) throw err;
+        for(let i = 0; i < results.length; i++) {
+            choices.push(`${results[i].first_name} ${results[i].last_name}`);
+        }
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: 'Select employee to remove',
+                name: 'userChoice',
+                choices: choices
+            }
+        ])
+        .then((results) =>{
+            const firstName = results.userChoice.split(' ')[0];
+            const lastName = results.userChoice.split(' ')[1];
+            connection.query('DELETE FROM employee where first_name = ? AND last_name = ?', [firstName, lastName], (err, results) =>{
+                if(err) throw err;
+                console.log(`Removed ${results.userChoice} from database`);
+                init();
+            })
+        })
+    })
+
+}
+
+const removeARole = () => {
+    let choices = [];
+    let ids = [];
+    connection.query('SELECT id, title from role', (err, results) => {
+        if(err) throw err;
+        for(let i = 0; i < results.length; i++) {
+            choices.push(results[i].title);
+            ids.push(results[i].id);
+        }
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: 'Select which role to remove',
+                name: 'userChoice',
+                choices: choices
+            }
+        ])
+        .then((results) => {
+            const roleIndex = choices.indexOf(results.userChoice);
+            const id = ids[roleIndex];
+            const role = results.userChoice;
+            
+            //write function to update employees whose role was deleted
+            connection.query('SELECT first_name, last_name FROM employee where role_id = ?', [id], (err, results) => {
+                if(err) throw err;
+                if(results.length === 0){
+                    connection.query('DELETE FROM role where title = ?', [role], (err, results) => {
+                        if(err) throw err;
+                        console.log(`Removed ${role} from roles`);
+                        init();
+                    })
+                } else {
+                    let employeeChoices = []
+                    for(let i = 0; i < results.length; i++){
+                        employeeChoices.push(`${results[i].first_name} ${results[i].last_name}`);
+                    };
+                    updateEmployeesRemovedRole(role, id, employeeChoices);
+                    
+                }
+            })
+            
+        })
+    })
+}
+
+const updateEmployeesRemovedRole = (role, removedID, employeesOfRemovedId) => {
+    console.log(role);
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: `You have may have employees whose role is ${role} and must update them. Would you like to update all or handle individually?`,
+            name: 'updatePrompt',
+            choices: ['Update all', 'Handle individually']
+        }
+    ])
+        .then((results) => {
+            if (results.updatePrompt === 'Update all') {
+                let choices = [];
+                let ids = [];
+
+                connection.query('SELECT id, title FROM role', (err, results) => {
+                    if (err) throw err;
+
+                    for (let i = 0; i < results.length; i++) {
+                        if (results[i].title !== role) {
+                            choices.push(results[i].title);
+                            ids.push(results[i].id);
+                        }
+                    }
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            message: 'Select role to which you want to update all employees who had the removed role',
+                            name: 'userResponse',
+                            choices: choices
+                        }
+                    ])
+                        .then((results) => {
+                            const roleIndex = choices.indexOf(results.userResponse);
+                            const id = ids[roleIndex];
+                            connection.query('UPDATE employee SET role_id = ? WHERE role_id = ?', [id, removedID], (err, results) => {
+                                if (err) throw err;
+                                console.log('Updated employees');
+                                // cb(results);
+                                // init();
+
+                                if(results.affectRows === 0){
+                                    console.log('Failed to update');
+                                } else {
+                                    connection.query('DELETE FROM role where title = ?', [role], (err, results) => {
+                                        if(err) throw err;
+                                        console.log(`Removed ${role} from roles`);
+                                        init();
+                                    })
+                                }
+                            });
+                        })
+                })
+            } else {
+                updateIndividualEmployeeRoles(role, employeesOfRemovedId);
+            }
+        });
+    
+    
+}
+
+const updateIndividualEmployeeRoles = (role, employeesOfRemovedId, cb) =>{
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Pick which employee to update',
+            name: 'userChoice',
+            choices: employeesOfRemovedId
+        }
+    ])
+        .then((results) => {
+            const firstName = results.userChoice.split(' ')[0];
+            const lastName = results.userChoice.split(' ')[1];
+            let index = employeesOfRemovedId.indexOf(results.userChoice);
+            employeesOfRemovedId.splice(index, 1);
+            let choices = [];
+            let ids = [];
+            connection.query('SELECT id, title FROM role', (err, results) => {
+                if (err) throw err;
+
+                for (let i = 0; i < results.length; i++) {
+                    if (results[i].title !== role) {
+                        choices.push(results[i].title);
+                        ids.push(results[i].id);
+                    }
+                }
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        message: 'Select role to which you want to update the chosen employee',
+                        name: 'userResponse',
+                        choices: choices
+                    }
+                ])
+                    .then((results) => {
+                        const roleIndex = choices.indexOf(results.userResponse);
+                        const id = ids[roleIndex];
+                        connection.query('UPDATE employee SET role_id = ? WHERE first_name = ? AND last_name = ?', [id, firstName, lastName], (err, results) => {
+                            if (err) throw err;
+                            console.log('Updated employees');
+                            // k++;
+                            // init();
+                            if(employeesOfRemovedId.length === 0) {
+                                    connection.query('DELETE FROM role where title = ?', [role], (err, results) => {
+                                        if(err) throw err;
+                                        console.log(`Removed ${role} from roles`);
+                                        init();
+                                    });
+                            }
+                            else{
+                                updateIndividualEmployeeRoles(role, employeesOfRemovedId);
+                            }
+                            // cb();
+                        });
+                    })
+            })
+        })    
+}
+
+const checkUserInputs = (...inputs) =>{
+    let check = false;
+    for(let i = 0; i < inputs.length; i++) {
+        let userInput = inputs[i]
+        if(userInput === '' || userInput.length > 30){
+            check = true;
+            return check;
+        }
+    }
+    return check;
+}
+
+
+init();
